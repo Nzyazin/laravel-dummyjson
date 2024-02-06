ad0f07890fe371cb33b9f9262e8e6cb00d14e043
Nzyazin <69486560+Nzyazin@users.noreply.github.com>
2024-02-06 14:35:55 +0300

    adding interface for repository

---
+++ app/Repositories/Interfaces/PostRepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories\Interfaces;
+
+interface PostRepositoryInterface
+{
+    public function importPosts(array $posts);
+
+    public function getLatestPostsWithComments(int $postsCount = 25, int $commentsCount = 3);
+}
