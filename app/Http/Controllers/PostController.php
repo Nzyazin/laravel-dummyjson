21d0cf8143523824c8f691e64c3a6b163644f787
Nzyazin <69486560+Nzyazin@users.noreply.github.com>
2024-02-07 00:03:19 +0300

    update controller and view welcome

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -23,12 +23,23 @@ class PostController extends Controller
 
     public function import()
     {
-        $skip = $this->postRepository->countPosts();
-        // Получаем 10 постов из внешнего сервиса
-        $posts = $this->dummyJsonService->getPosts($limit = 10, $skip,  $select = 'title,body,userId,tags,reactions');
-        $this->postRepository->importPosts($posts);
-        //dd($skip);              
-        //return redirect('/posts')->with('success', 'Posts imported successfully');
+        try {
+            // Подсчитываем уже имеющееся количество постов
+            $skip = $this->postRepository->countPosts();
+            // Получаем 10 постов из внешнего сервиса
+            $posts = $this->dummyJsonService->getPosts($limit = 10, $skip, $select = 'title,body,userId,tags,reactions');
+    
+            if ($posts) {
+                $this->postRepository->importPosts($posts);
+                return redirect('/')->with('success', 'Posts imported successfully');
+            } else {
+                // Если посты не были получены, возвращаемся с ошибкой
+                return redirect('/')->with('error', 'No posts to import');
+            }
+        } catch (\Exception $e) {
+            // В случае возникновения исключения возвращаемся с ошибкой
+            return redirect('/')->with('error', 'Failed to import posts');
+        }
     }
 
     public function index()
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,17 @@
 <body>
     <h1>Welcome to our application!</h1>
     <p>Click the button below to import posts and comments.</p>
+    @if (session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+    @endif
+
+    @if (session('error'))
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
+    @endif
     <a href="{{ url('/import') }}" class="btn btn-primary">Import Posts</a>
 </body>
 </html>
