79ce6f9409c585a2f16ad24d0035150fa7a37974
Nzyazin <69486560+Nzyazin@users.noreply.github.com>
2024-02-06 14:19:20 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/import', 'PostController@import');
+Route::get('/posts', 'PostController@index');
