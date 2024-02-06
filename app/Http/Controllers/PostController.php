6fa7b188e412bba9b883a2004b8c54ad3d49f191
Nzyazin <69486560+Nzyazin@users.noreply.github.com>
2024-02-06 23:34:23 +0300

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,6 +6,7 @@ use App\Repositories\Interfaces\PostRepositoryInterface;
 use App\Repositories\Interfaces\CommentRepositoryInterface;
 use App\Services\DummyJsonService; // Предполагается, что этот сервис реализует логику запросов к внешнему API
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -22,14 +23,16 @@ class PostController extends Controller
 
     public function import()
     {
-        $posts = $this->dummyJsonService->getPosts(10); // Получаем 10 постов из внешнего сервиса
-
-
-        return redirect('/posts')->with('success', 'Posts imported successfully');
+        $skip = $this->postRepository->countPosts();
+        // Получаем 10 постов из внешнего сервиса
+        $posts = $this->dummyJsonService->getPosts($limit = 10, $skip,  $select = 'title,body,userId,tags,reactions');
+        $this->postRepository->importPosts($posts);
+        //dd($skip);              
+        //return redirect('/posts')->with('success', 'Posts imported successfully');
     }
 
     public function index()
-    {       
+    {
 
         // Передаем посты в вид
         return view('posts.index', compact('posts'));
@@ -40,4 +43,3 @@ class PostController extends Controller
         return view('welcome');
     }
 }
-
