1bf6274dcc08dcf696c25d093ddddf3e267671ac
Nzyazin <69486560+Nzyazin@users.noreply.github.com>
2024-02-06 23:33:25 +0300

    Update PostRepositoryInterface.php

--- app/Repositories/Interfaces/PostRepositoryInterface.php
+++ app/Repositories/Interfaces/PostRepositoryInterface.php
@@ -7,4 +7,6 @@ interface PostRepositoryInterface
     public function importPosts(array $posts);
 
     public function getLatestPostsWithComments(int $postsCount = 25, int $commentsCount = 3);
+
+    public function countPosts();
 }
