07f21ce6472cba61c9b500e2bafa6612692d4393
Nzyazin <69486560+Nzyazin@users.noreply.github.com>
2024-02-06 23:34:17 +0300

    update repositories

--- app/Repositories/CommentRepository.php
+++ app/Repositories/CommentRepository.php
@@ -5,19 +5,36 @@ namespace App\Repositories;
 use App\Models\Comment;
 use App\Models\Post;
 use App\Repositories\Interfaces\CommentRepositoryInterface;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Log;
 
 class CommentRepository implements CommentRepositoryInterface
 {
     public function importComments($postId, array $comments)
-    {
-        $post = Post::find($postId);
-
+    {        
+        
         foreach ($comments as $comment) {
-            $post->comments()->create([
-                'external_id' => $comment['id'],
-                'body' => $comment['body'],
-                // Дополнительные поля...
-            ]);
+            $existingComment = Comment::where('id', $comment['id'])->first();
+            if (!$existingComment) {
+                $validator = Validator::make($comment, [
+                    'id' => 'required|numeric|unique:posts',
+                    'body' => 'required|string',
+                    'post_id' => 'required|numeric',
+                    'user_id' => 'required|numeric'
+                ]);
+
+                if ($validator->fails()) {
+                    return response()->json(['errors' => $validator->errors()], 422);
+                }
+
+                Comment::create([
+                    'id' => $comment['id'],
+                    'body' => $comment['body'],
+                    'post_id' => $comment['postId'],
+                    'user_id' => $comment['user']['id']
+                ]);
+            }
+            
         }
     }
 }
--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -4,22 +4,45 @@ namespace App\Repositories;
 
 use App\Models\Post;
 use App\Repositories\Interfaces\PostRepositoryInterface;
+use Illuminate\Support\Facades\Validator;
+use App\Services\DummyJsonService;
+use App\Repositories\CommentRepository;
 
 class PostRepository implements PostRepositoryInterface
-{
+{    
+
     public function importPosts(array $posts)
-    {
-        foreach ($posts as $post) {
-            $existingPost = Post::where('external_id', $post['id'])->first();
+    {           
+        foreach ($posts['posts'] as $post) {
+            $existingPost = Post::where('id', $post['id'])->first();
 
             if (!$existingPost) {
+                $validator = Validator::make($post, [
+                    'id' => 'required|numeric|unique:posts',
+                    'title' => 'required|string',
+                    'body' => 'required|string',
+                    'userId' => 'required|numeric',
+                    'tags' => 'nullable|array',
+                    'reactions' => 'nullable|numeric',
+                ]);
+
+                if ($validator->fails()) {
+                    return response()->json(['errors' => $validator->errors()], 422);
+                }
+
                 Post::create([
-                    'external_id' => $post['id'],
+                    'id' => $post['id'],
                     'title' => $post['title'],
                     'body' => $post['body'],
-                    // Дополнительные поля...
+                    'userId' => $post['userId'],
+                    'tags' => json_encode($post['tags']),
+                    'reactions' => $post['reactions']
                 ]);
             }
+            $dummyService = new DummyJsonService();
+            $commentsPost = $dummyService->getCommentsByPost($post['id']);
+            $commentRepository = new CommentRepository();
+            $commentRepository->importComments($post['id'], $commentsPost['comments']);
         }
     }
 
@@ -28,5 +51,11 @@ class PostRepository implements PostRepositoryInterface
         return Post::with(['comments' => function ($query) use ($commentsCount) {
             $query->latest()->take($commentsCount);
         }])->latest()->take($postsCount)->get();
+    }
+
+    public function countPosts()
+    {
+        $postsCount = Post::count();
+        return $postsCount;
     }
 }
