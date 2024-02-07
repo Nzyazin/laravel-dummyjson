0821579fd81eec6cf33164a07a2f7ec95c187b49
Nzyazin <69486560+Nzyazin@users.noreply.github.com>
2024-02-07 11:20:15 +0300

    update Models

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -7,12 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    protected $fillable = ['id', 'body', 'post_id', 'user_id'];
+    protected $fillable = ['id', 'body', 'postId', 'user'];
     /**
      * Получить пост, к которому относится этот комментарий.
      */
     public function post()
     {
-        return $this->belongsTo(Post::class, 'post_id');
+        return $this->belongsTo(Post::class, 'postId');
     }
 }
